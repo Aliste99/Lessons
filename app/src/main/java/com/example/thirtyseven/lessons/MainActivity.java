9c692acace50110607230762a930c18bd5040027
Aliste99 <alechka9906@gmail.com>
2017-03-15 18:32:24 +0600

    Add bottomNavigationView

--- app/src/main/java/com/example/thirtyseven/lessons/MainActivity.java
+++ app/src/main/java/com/example/thirtyseven/lessons/MainActivity.java
@@ -3,17 +3,67 @@ package com.example.thirtyseven.lessons;
 import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Button button;
+    private TextView textGroup;
+    private TextView textReplacement;
+    private TextView textSchedule;
+    private TextView textProfile;
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_mygroup:
+                    textGroup.setVisibility(View.VISIBLE);
+                    textReplacement.setVisibility(View.GONE);
+                    textSchedule.setVisibility(View.GONE);
+                    textProfile.setVisibility(View.GONE);
+                    return true;
+                case R.id.navigation_replacement:
+                    textGroup.setVisibility(View.GONE);
+                    textReplacement.setVisibility(View.VISIBLE);
+                    textSchedule.setVisibility(View.GONE);
+                    textProfile.setVisibility(View.GONE);
+                    return true;
+                case R.id.navigation_schedule:
+                    textGroup.setVisibility(View.GONE);
+                    textReplacement.setVisibility(View.GONE);
+                    textSchedule.setVisibility(View.VISIBLE);
+                    textProfile.setVisibility(View.GONE);
+                    return true;
+                case R.id.navigation_profile:
+                    textGroup.setVisibility(View.GONE);
+                    textReplacement.setVisibility(View.GONE);
+                    textSchedule.setVisibility(View.GONE);
+                    textProfile.setVisibility(View.VISIBLE);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        initVars();
+        setListeners();
         if(!isUser()){
             //TODO переходит intentом в другой activity
         }
@@ -30,6 +85,30 @@ public class MainActivity extends AppCompatActivity {
         WeekDate weekDate = new WeekDate();
         setDate();
         setOddOrNot();
+
+    }
+
+    private void setListeners() {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*switch (v.getId()){
+                    case R.id.button2:
+                        Intent intent1 = new Intent(MainActivity.this, Main2Activity.class);
+                        startActivity(intent1);
+                        break;
+                }*/
+            }
+        };
+
+
+    }
+
+    private void initVars() {
+        textGroup = (TextView)findViewById(R.id.text_mygroup);
+        textReplacement= (TextView)findViewById(R.id.text_replacement);
+        textSchedule = (TextView)findViewById(R.id.text_schedules);
+        textProfile = (TextView)findViewById(R.id.text_profile);
 
     }
 
